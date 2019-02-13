44753a3d1b65b73f546520b2cb2498c550108017
EvertonGhth <47040861+EvertonGhth@users.noreply.github.com>
2019-02-13 17:21:31 -0200

    Add files via upload

---
+++ Aula 2 - FOR(2).cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+int count;
+for (count=1;count<=100;count=2*count+1) 
+printf ("%d ",count);
+
+}
---
+++ media.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+	float av1, av2;
+	
+	printf("Digite a nota da AV1:\n");
+	scanf("%f",&av1);
+	printf("Digite a nota da AV2:\n");
+	scanf("%f",&av2);
+	printf("A media e: %.1f",(av1+av2)/2);
+}
